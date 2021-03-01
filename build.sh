df8129e3cb1621e90946e304ce3b89e5b4bd2b6a
Wilson Gramer <12683684+WilsonGramer@users.noreply.github.com>
2021-03-01 08:20:49 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ set -e
 host="$(rustc --version --verbose | grep host: | cut -c 7-)"
 
 cargo build --release
-cp target/release/libplugin.dylib "$host.wplplugin"
+cp target/release/libplugin.{dylib,so} "$host.wplplugin"
