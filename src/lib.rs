2d89cabc76d37e0964dc53b914d28b4dd6fde288
Wilson Gramer <wilson@gramer.dev>
2021-03-02 17:45:49 -0500

    Update Wipple version

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use wipple::*;
 use wipple_plugin_loader::wipple_plugin;
 
-wipple_plugin!(|env: &mut Environment, stack: &Stack| {
+wipple_plugin!(|env: &EnvironmentRef, stack: &Stack| {
     Ok(Value::of(Text {
         text: String::from("Hello, world!"),
         location: None,
