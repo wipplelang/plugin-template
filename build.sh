cefcd93694106d3b2130d295a463c45e74da4147
Wilson Gramer <wilson@gramer.dev>
2021-03-06 13:32:04 -0500

    Fix build script

--- build.sh
+++ build.sh
@@ -5,4 +5,7 @@ set -e
 host="$(rustc --version --verbose | grep host: | cut -c 7-)"
 
 cargo build --release
-cp target/release/libplugin.{dylib,so} "$host.wplplugin"
+
+for f in $(find target/release -maxdepth 1 -name libplugin.dylib -o -name libplugin.so); do
+    cp $f "$host.wplplugin"
+done
